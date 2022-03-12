3bd9a4b634f6e9592a2b4084773def68e8e5ba0a
eleskin <eleskin1201@gmail.com>
2022-03-12 21:54:18 +0300

    feature: Navigation component is done

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,69 @@
+import styles from './Navigation.module.css';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
+
+import navigation_image_1 from '../../assets/images/navigation/navigation-icon-1.svg';
+import navigation_image_2 from '../../assets/images/navigation/navigation-icon-2.svg';
+import navigation_image_3 from '../../assets/images/navigation/navigation-icon-3.svg';
+import navigation_image_4 from '../../assets/images/navigation/navigation-icon-4.svg';
+import navigation_image_5 from '../../assets/images/navigation/navigation-icon-5.svg';
+import navigation_image_6 from '../../assets/images/navigation/navigation-icon-6.svg';
+import navigation_image_7 from '../../assets/images/navigation/navigation-icon-7.svg';
+
+const Navigation: Function = (): JSX.Element => {
+	return (
+		<div className={styles.Navigation}>
+			<span className={styles.Navigation__label}>SERVICES</span>
+			<ul className={styles.Navigation__list}>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_1} alt=""/> Dashboard</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_2} alt=""/> Google Ad</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_3} alt=""/> SEO rank</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_4} alt=""/> SEO reports</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_5} alt=""/> SMM center</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+			</ul>
+			<span className={styles.Navigation__label}>OTHERS</span>
+			<ul className={styles.Navigation__list}>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_6} alt=""/> Payments</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+				<li>
+					<a href="/">
+						<span><img src={navigation_image_7} alt=""/> Help center</span>
+						<i><FontAwesomeIcon icon={faChevronRight}/></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Navigation;
