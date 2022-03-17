ca2fe1e109d5a46bf740bfb4c4c3d4d8ca357672
eleskin <eleskin1201@gmail.com>
2022-03-17 12:11:17 +0300

    fix: Metric test name

--- src/components/Metric/Metric.test.tsx
+++ src/components/Metric/Metric.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import Metric from './Metric';
 
-test('Logo render', () => {
+test('Metric render', () => {
 	render(<Metric title="CALLS" value="150" changing={-12}/>);
 	const linkElement = screen.getByText(/CALLS/i);
 	expect(linkElement).toBeInTheDocument();
