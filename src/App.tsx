a1ee5a126ed3091410b6c1e31fa6e3e1c7e3c4aa
eleskin <eleskin1201@gmail.com>
2022-03-23 11:39:20 +0300

    feature: add adaptive to Wizard component

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import Wizard from './components/Wizard/Wizard';
 
 const App: Function = (): JSX.Element => {
 	const [isHovering, setIsHovering]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(Boolean(false));
+	const [isVisibleWizard, setIsVisibleWizard]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(Boolean(true));
 	
 	return (
 		<div className={styles.App}>
 			<Navbar isHovering={isHovering} setIsHovering={setIsHovering}/>
 			<Main setIsHovering={setIsHovering}/>
-			<Wizard/>
+			<Wizard isVisibleWizard={isVisibleWizard} setIsVisibleWizard={setIsVisibleWizard}/>
 		</div>
 	);
 };
