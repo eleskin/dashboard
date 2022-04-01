bb658bab961a29124b0c65f059fb384d128aae29
eleskin <eleskin1201@gmail.com>
2022-04-01 15:42:46 +0300

    feature: Register component is done

--- src/views/Register/Register.test.tsx
+++ src/views/Register/Register.test.tsx
@@ -1,8 +1,17 @@
 import {render, screen} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import store from '../../store';
 import Register from './Register';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 test('Register render', () => {
-	render(<Register/>);
+	render(
+		<Provider store={store}>
+			<Router>
+				<Register/>
+			</Router>
+		</Provider>,
+	);
 	const firstElement = screen.getAllByText(/Please fill in the form fields/i)[0] as HTMLAnchorElement;
 	expect(firstElement).toBeInTheDocument();
 });
